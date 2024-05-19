0ccadf63206f4a2905f8d469e5d428a0eda49752
Churamani budhathoki <155580704+churamanib@users.noreply.github.com>
2024-05-19 01:42:46 -0400

    Update Linux_Setup.sh

--- Linux_Setup.sh
+++ Linux_Setup.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
+OR='\e[38;5;202m'
+GR='\e[32m'
+NL='\e[0m'
+WH='\e[97m'
+BL='\e[34m'
+
+
+echo -e "
+${OR}  666666  66     66  66        66   666666        6        666          666      6        666      66  66666666       ${NL} 
+${OR} 66       66     66  66        66  66     6     66  66     66  66    66  66    66 66      66  6    66     66               ${NL}
+${WH}66        66 666 66  66        66  66    66    66    66    66     66     66   66    66    66   6   66     66        ${NL}
+${WH}66        66 666 66  66        66  66 666     6666666666   66            66  6666666666   66    6  66     66         ${NL}  
+${GR} 66       66     66   66      66   66    66   66       66  66            66  66       66  66     6 66     66       ${NL}
+${GR}  666666  66     66    6666666     66     66 66         66 66            66 66         66 66       666 66666666 ${NL}
+"
+echo "  hello friend this is Churamani budhathoki ethical hacker (my nikname and public name (Amar))
+"
+# Check if Java JDK 19 is installed
+
 if [[ $EUID -eq 0 ]]; then
     # Download Burp Suite Profesional Latet Version
     echo 'Downloading Burp Suite Professional v2022.8.2 ....'
